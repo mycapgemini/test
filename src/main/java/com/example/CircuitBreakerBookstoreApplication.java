c35047f57e036bca32e4188e1e0bf231f05d5b6d
mycapgemini <niraj.g.kumar@capgemini.com>
2017-02-20 12:14:25 +0530

    first commit

---
+++ src/main/java/com/example/CircuitBreakerBookstoreApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerBookstoreApplication.class, args);
+	}
+}
